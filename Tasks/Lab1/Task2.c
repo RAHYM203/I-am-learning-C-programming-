382f0eac395b68648fe0ab1b483ed28f40344a5b
RAHYM203 <119367622+RAHYM203@users.noreply.github.com>
2025-06-03 18:30:07 +0200

    Write a program that prints the message "I like programming in C!!!" on the screen
    Write a program that prints the message "I like programming in C!!!" on the screen

---
+++ Tasks/Lab1/Task2.c
@@ -0,0 +1,10 @@
+/* Task2 Write a program that prints the message "I like programming in C!!!" on the screen. */
+
+#include<stdio.h>
+
+int main(void){
+
+printf("I like programming in C!!!");
+  
+return 0;
+}
